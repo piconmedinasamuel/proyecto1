d198b4530f397ba84daff2f23e7d54b6fa128d89
piconmedinasamuel <71028839+piconmedinasamuel@users.noreply.github.com>
2021-07-21 16:53:00 -0500

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,10 @@
+//programa que nos ayude a restablecer
+//el costo total de una consola de video juegos
+
+//en el exterior
+//compra en dolares
+//pagar en un casillero
+//(dolares costo dependiendo de la cantidad de productos)
+//(envio hasta colombia
+//(cantidad de productos)
+//pagar inpuestos de entrada al pais (lo digita aduanas)
